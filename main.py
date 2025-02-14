8ca43fce05a05ed8e53af7a1b6e70df780c5c629
Snakehater <vigor.turujlija@gmail.com>
2025-02-14 17:08:30 +0100

    Added filename argument

--- main.py
+++ main.py
@@ -484,13 +484,17 @@ def main():
     parser = argparse.ArgumentParser(description="Process MP3 and generate Nothing-compatible OGG files or visualize audio.")
     parser.add_argument("-c", action="store_true", help="Create an OGG file for Nothing Glyph Composer")
     parser.add_argument("-i", action="store_true", help="Inspect CUSTOM1 data when used with -c")
+    parser.add_argument("filename")
     args = parser.parse_args()
+    if (len(args.filename.split(".")) != 2):
+        print(args.filename.split("."))
+        print("Error file")
+        exit(1)
     
-    mp3_file = "pedro.mp3"
     wav_file = "temp.wav"
-    output_file = "pedro_nothing.ogg"
+    output_file = f"{args.filename.split('.')[0]}.ogg"
     
-    convert_mp3_to_wav(mp3_file, wav_file)
+    convert_mp3_to_wav(args.filename, wav_file)
     
     if args.c:
         generate_nothing_ogg(wav_file, output_file, inspect=args.i)
---
+++ yt_download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -m yt_dlp -x --audio-format mp3 -f bestaudio $URL
